1e58615b7ef73279d320f4cb97abbd39a49e5afb
nehru <nehruyadav729@gmail.com>
2022-07-10 12:30:45 +0530

    factorial

--- problem1.js
+++ problem1.js
@@ -1,10 +1,16 @@
   
         function Factorial(n) {
+            //check the condition
             if (n === 0) {
                 return 1;
             }
+            //condtion is fails it enters to else block and execute the statements
             else {
                 return n * Factorial( n - 1 );
             }
         }
         console.log(Factorial(5));
+
+
+        //input is 5
+        //output is 120
