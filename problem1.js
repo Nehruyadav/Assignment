379c66f2a03e552fca758ea07ac648b57df61d90
nehru <nehruyadav729@gmail.com>
2022-07-10 12:28:41 +0530

    factorial

--- problem1.js
+++ problem1.js
@@ -1,1 +1,10 @@
-console.log(5);
+  
+        function Factorial(n) {
+            if (n === 0) {
+                return 1;
+            }
+            else {
+                return n * Factorial( n - 1 );
+            }
+        }
+        console.log(Factorial(5));
